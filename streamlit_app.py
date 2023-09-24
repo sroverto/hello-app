fa7a6bb922aeec00c7a1a66637a1b166836bf492
sroverto <116098476+sroverto@users.noreply.github.com>
2023-09-24 13:56:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,18 +17,28 @@ data.add_series("Baz", [5, 3, 2])
 
 story = Story(data=data)
 
+# create Slides and Steps and add them to the Story
 slide1 = Slide(
     Step(
         Config({"x": "Foo", "y": "Bar"}),
+        )
     )
-)
 story.add_slide(slide1)
-
 slide2 = Slide(
-    Step(
-        Config({"color": "Foo", "x": "Baz", "geometry": "circle"}),
+    Step(Config({"color": "Foo", "x": "Baz", "geometry": "circle"}))
     )
-)
 story.add_slide(slide2)
 
+# note: in Streamlit if you want to use the `play` method,
+# you need to set the width and height in pixels
+story.set_size(width="800px", height="480px")
+
+# you can export the Story into a html file
+story.export_to_html(filename="mystory.html")
+
+# or you can get the html Story as a string
+html = story.to_html()
+print(html)
+
+# you can display the Story with the `play` method
 story.play()
