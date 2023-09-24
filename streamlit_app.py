e465a337a770c0825513c0e749f7aed627f47e0e
sroverto <116098476+sroverto@users.noreply.github.com>
2023-09-24 13:52:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from ipyvizzu import Data, Config
+from ipyvizzustory import Story, Slide, Step
+
 
 st.title("Streamlit Tutorial App")
 st.write("This is my new app")
@@ -6,3 +9,26 @@ st.write("This is my new app")
 button1 = st.button("Click me")
 if button1:
     st.write("This is some text")
+
+data = Data()
+data.add_series("Foo", ["Alice", "Bob", "Ted"])
+data.add_series("Bar", [15, 32, 12])
+data.add_series("Baz", [5, 3, 2])
+
+story = Story(data=data)
+
+slide1 = Slide(
+    Step(
+        Config({"x": "Foo", "y": "Bar"}),
+    )
+)
+story.add_slide(slide1)
+
+slide2 = Slide(
+    Step(
+        Config({"color": "Foo", "x": "Baz", "geometry": "circle"}),
+    )
+)
+story.add_slide(slide2)
+
+story.play()
